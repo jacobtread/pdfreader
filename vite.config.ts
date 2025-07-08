c562a2239557f09edd7f21dfcfcaf5a0938e6e88
Jacobtread <jacobtread@gmail.com>
2025-07-09 02:03:57 +1200

    feat: library handling and building fixes and improvements

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,30 @@ export default defineConfig({
   build: {
     lib: {
       entry: "src/index.ts",
+      name: "PDFReader",
       formats: ["es", "cjs"],
+      fileName: (format) => `pdfreader.${format}.js`,
     },
     emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
-      external: ["react", "react-dom", "pdfjs-dist", "react/jsx-runtime"],
+      output: {
+        exports: "named",
+        preserveModules: true,
+        preserveModulesRoot: "src",
+        entryFileNames: "[name].[format].js",
+        chunkFileNames: "[name].[format].js",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
+      external: (id) => {
+        return ["react", "react-dom", "react/jsx-runtime"].some(
+          (pkg) => id === pkg || id.startsWith(pkg + "/"),
+        );
+      },
     },
   },
   plugins: [react()],
